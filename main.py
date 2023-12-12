e66db09f6b29bc7e79baf7a6141bebdd2c1adfcc
Nick Kelly <k05nk01@gmail.com>
2023-12-11 17:35:11 -0800

    Added ability to go again

--- main.py
+++ main.py
@@ -4,27 +4,55 @@
 BEEF_KG_CO2_PER_KG = 40.2
 GASOLINE_CO2_PER_GALLON = 8.887
 
-choice = input("Did you drive or did you eat beef? Please type 'drive' or 'beef'.\n").lower()
+keep_going = True
 
-match choice:
-    case "drive":
-        distance = float(input("How many miles did you drive? Please enter a number.\n"))
-        vehicle_mpg = float(input("What mpg does your car get? Please enter a number.\n"))
+while keep_going:
 
-        gas_used = distance / vehicle_mpg
-        CO2_emitted_driving = float("{:.2f}".format(gas_used * GASOLINE_CO2_PER_GALLON))
-        beef_equivalent = float("{:.2f}".format(CO2_emitted_driving / BEEF_KG_CO2_PER_KG))
+    choice = input("Did you drive or did you eat beef? Please type 'drive' or 'beef'.\n").lower()
 
-        print(f"Your drive emitted {CO2_emitted_driving} kg of CO2. You could have eaten {beef_equivalent} kg of beef!")
+    match choice:
+        case "drive":
+            distance = float(input("How many miles did you drive? Please enter a number.\n"))
+            vehicle_mpg = float(input("What mpg does your car get? Please enter a number.\n"))
 
-    case "beef":
-        beef_eaten = float(input("How much beef did you eat, in grams?\n"))
-        CO2_of_beef_eaten = float("{:.2f}".format((beef_eaten/1000) * BEEF_KG_CO2_PER_KG))
-        vehicle_mpg = float(input("What mpg does your car get? Please enter a number.\n"))
+            gas_used = distance / vehicle_mpg
+            CO2_emitted_driving = float("{:.2f}".format(gas_used * GASOLINE_CO2_PER_GALLON))
+            beef_equivalent = float("{:.2f}".format(CO2_emitted_driving / BEEF_KG_CO2_PER_KG))
 
-        distance_possibly_driven = float("{:.2f}".format((CO2_of_beef_eaten / GASOLINE_CO2_PER_GALLON)*vehicle_mpg))
+            print(f"Your drive emitted {CO2_emitted_driving} kg of CO2. You could have eaten {beef_equivalent} kg of beef.")
 
-        print(f"The beef you ate emitted {CO2_of_beef_eaten} kg of CO2. You could have driven your car {distance_possibly_driven}"
-              f" miles for the same emissions.")
+            while True:
+                go_again = input("Would you like to go again? Please enter 'Y' or 'n'.\n")
+                match go_again:
+                    case 'Y':
+                        break
+                    case 'n':
+                        keep_going = False
+                        break
+                    case _:
+                        print("Please enter 'Y' or 'n'.")
 
+        case "beef":
+            beef_eaten = float(input("How much beef did you eat, in grams?\n"))
+            CO2_of_beef_eaten = float("{:.2f}".format((beef_eaten/1000) * BEEF_KG_CO2_PER_KG))
+            vehicle_mpg = float(input("What mpg does your car get? Please enter a number.\n"))
+
+            distance_possibly_driven = float("{:.2f}".format((CO2_of_beef_eaten / GASOLINE_CO2_PER_GALLON)*vehicle_mpg))
+
+            print(f"The beef you ate emitted {CO2_of_beef_eaten} kg of CO2. You could have driven your car {distance_possibly_driven}"
+                  f" miles for the same emissions.")
+
+            while True:
+                go_again = input("Would you like to go again? Please enter 'Y' or 'n'.\n")
+                match go_again:
+                    case 'Y':
+                        break
+                    case 'n':
+                        keep_going = False
+                        break
+                    case _:
+                        print("Please enter 'Y' or 'n'.")
+
+        case _:
+            print("That options isn't currently supported. Please select one of the supported options.")
 
