b208681e6e2b984a5425ba2af03f9c7ffc43c5d9
Nick Kelly <k05nk01@gmail.com>
2024-04-24 14:22:23 -0700

    Changed the home prompt and added butter as an option

--- co2_impacts.py
+++ co2_impacts.py
@@ -2,15 +2,16 @@
 
 
 co2_impacts = {
-  "beef": 40.2,
-  "chicken": 6.9,
-  "coffee": 0.4,
-  "rice": 0.9,
-  "gasoline": 8.887,
-  "honey": 1.4,
-  "peanut butter": 2.88,
-  "bread slice": (.0589 / 16), # (one loaf contains 16 slices, roughly)
-  "cottage cheese": 7.75,
-  "eggs": (2.7 / 12), # 2.7kg per dozen eggs
-  "english muffin": (1.38 * 0.061), # from ClimateHub. Reported in CO2/ kg, and an english muffin is 61g per the package.https://apps.carboncloud.com/climatehub/product-reports/072220002751/USA
+  "Beef (Kg)": 40.2,
+  "Butter, salted (Kg)": 10.179, # https://livelca.com/products/butter_273d0577-1c3b-4e5e-94fb-4084b0eebe78
+  "Chicken (Kg)": 6.9,
+  "Coffee (cups)": 0.4,
+  "Rice (Kg)": 0.9,
+  "Gasoline (US gal)": 8.887,
+  "Honey (Kg)": 1.4,
+  "Peanut Butter (Kg)": 2.88,
+  "Bread (slices)": (.0589 / 16), # (one loaf contains 16 slices, roughly)
+  "Cottage Cheese": 7.75,
+  "Eggs, Large (qty eggs)": (2.7 / 12), # 2.7kg per dozen eggs
+  "English Muffin (qty of muffins)": (1.38 * 0.061), # from ClimateHub. Reported in CO2/ kg, and an english muffin is 61g per the package.https://apps.carboncloud.com/climatehub/product-reports/072220002751/USA
 }
