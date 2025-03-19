6a8d1c7faf40e91895b520f06463b1c5699e3734
Nick Kelly <k05nk01@gmail.com>
2025-03-19 16:32:12 -0700

    Started adding tests

--- test/test_main.py
+++ test/test_main.py
@@ -1,7 +1,21 @@
 # content of test_sample.py
-def func(x):
-    return x + 1
+# def func(x):
+#     return x + 1
+#
+#
+# def test_answer():
+#     assert func(3) == 5
 
 
-def test_answer():
-    assert func(3) == 5
+from main import calc_co2_emitted_driving, calc_co2_beef, calc_co2_chicken,\
+    calc_co2_coffee
+
+
+def test_co2_emitted_driving():
+    assert calc_co2_emitted_driving(100,45) == 19.75
+
+def test_co2_beef():
+    assert calc_co2_beef(1) == 40.2
+
+def test_co2_chicken():
+    assert calc_co2_chicken(1) == 6.9
