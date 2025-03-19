9a1652758c253bf58bffe4f1656469331e7e7604
Nick Kelly <k05nk01@gmail.com>
2025-03-19 16:39:39 -0700

    Added more unit tests for functions that call external dicts.

--- test/test_main.py
+++ test/test_main.py
@@ -7,15 +7,32 @@
 #     assert func(3) == 5
 
 
-from main import calc_co2_emitted_driving, calc_co2_beef, calc_co2_chicken,\
-    calc_co2_coffee
+from main import calc_co2_emitted_driving,\
+    calc_co2_beef,\
+    calc_co2_chicken,\
+    calc_co2_coffee,\
+    calc_beef_equivalent,\
+    calc_vehicle_equivalent,\
+    calc_chicken_equivalent
 
 
-def test_co2_emitted_driving():
+def test_calc_co2_emitted_driving():
     assert calc_co2_emitted_driving(100,45) == 19.75
 
-def test_co2_beef():
+def test_calc_co2_beef():
     assert calc_co2_beef(1) == 40.2
 
-def test_co2_chicken():
+def test_calc_co2_chicken():
     assert calc_co2_chicken(1) == 6.9
+
+def test_calc_co2_coffee():
+    assert calc_co2_coffee(10) == 0.07
+
+def test_calc_chicken_equivalent():
+    assert calc_chicken_equivalent(10) == 1.45
+
+def test_calc_beef_equivalent():
+    assert calc_beef_equivalent(10) == 0.25
+
+def test_calc_vehicle_equivalent():
+    assert calc_vehicle_equivalent(100, 45) == 555.56
